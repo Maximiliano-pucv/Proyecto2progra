eb6ff76a281f882b223d27703c95b8d18c9be231
Max <127501320+Maximiliano-pucv@users.noreply.github.com>
2023-10-27 16:47:51 -0300

    Freestyle

---
+++ proyectoJuegoNaves2-core/src/com/mygdx/game/BallT.java
@@ -0,0 +1,9 @@
+package com.mygdx.game;
+
+public class BallT extends Ball2 implements PerfilEnemigo{
+	public void setVida(int v) {
+		vida = v;
+	}
+	
+	
+}
---
+++ proyectoJuegoNaves2-core/src/com/mygdx/game/PerfilEnemigo.java
@@ -0,0 +1,5 @@
+package com.mygdx.game;
+
+public interface PerfilEnemigo {
+	public void setVida(int V);
+}
